947b5b3f0c570866ec0f3521feff2df9ae207b7c
jiabochao <jiabochao@live.com>
2024-02-13 22:15:35 +0800

    feat: pdf export

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,13 @@ ENV TZ="Asia/Shanghai"
 COPY . .
 
 # 如果各公司有自己的私有源，可以替换registry地址
+RUN npx -y playwright install --with-deps
+
+RUN npm i -g playwright-chromium  -y
+RUN npm i -g @slidev/cli  -y
+RUN npm i -g @slidev/theme-seriph  -y
+RUN npm i -g @slidev/theme-default  -y
+
 RUN npm install
 
 RUN npm run build
--- src/controller/home.controller.ts
+++ src/controller/home.controller.ts
@@ -1,9 +1,23 @@
 import { Controller, Get } from '@midwayjs/core';
+import * as fs from 'fs';
+import * as child_process from 'child_process';
+import * as util from 'util';
+
+const exec = util.promisify(child_process.exec);
 
 @Controller('/')
 export class HomeController {
   @Get('/')
   async home(): Promise<string> {
     return 'Hello Midwayjs!';
+  }
+  @Get('/build')
+  async build() {
+    const { stdout, stderr } = await exec(
+      'slidev export slides.md --output my-pdf-export'
+    );
+    console.log(stdout);
+    console.log(stderr);
+    return fs.createReadStream('./my-pdf-export.pdf');
   }
 }
